d8ef88b3d13907a053357b3dc867992488db61f9
personnna <kadyelyer@gmail.com>
2025-12-11 14:08:44 +0100

    Initial Commit

---
+++ calloryCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calloryCounter
+//
+//  Created by ellkaden on 11.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
